a1ee4ffdbb211e8a1e4a720ef040203dd5fe26ad
shinakolisetti <shinakolisetti@gmail.com>
2014-12-12 13:28:16 -0500

    adding makefile and test.c

--- mymalloc.c
+++ mymalloc.c
@@ -1,10 +1,11 @@
 #include "mymalloc.h"
 
+
 #define MEMBLOCK_SIZE (5000)
 
 char memory_block[MEMBLOCK_SIZE];
 
-void* memMalloc( unsigned int s){
+void* memMalloc(unsigned int s, char * fileName, unsigned int lineNum){
 	static int initial = 1;
 	static MemBlock *head;
 	MemBlock *curr, *new;
@@ -14,19 +15,20 @@ void* memMalloc( unsigned int s){
 		head = (MemBlock*)memory_block;
 		head->prev = head->next = head;
 		head->free = 1;
+		head->size = MEMBLOCK_SIZE - sizeof(MemBlock);
 	}
 
-	curr = head;
+	curr = head->prev;
 	unsigned int newSize = s + sizeof(MemBlock);	
-
+	printf("-\n");
 	do{
-		if(curr->free && curr->size = s){
+		if(curr->free && curr->size == s){
 			curr->free = 0;
-			return (void *)(curr + sizeof(MemBlock);
+			printf("malloced new block of size %d at %p\n",s,((char*)curr + sizeof(MemBlock)));
+			return (void *)((char*)curr + sizeof(MemBlock));
 		}else if(curr->free && curr->size >= newSize){
 			curr->size -= newSize;
-			new = (MemBlock*) (curr + sizeof(MemBlock) + curr->size)
-
+			new = (MemBlock*) ((char*)(curr + sizeof(MemBlock)) + curr->size);
 			new->next = curr->next;
 			new->next->prev = new;
 			curr->next = new;
@@ -34,51 +36,66 @@ void* memMalloc( unsigned int s){
 
 			new->size = s;
 			new->free = 0;
-	
-			return (void *)(new + sizeof(MemBlock);
+			printf("malloced new block of size %d at %p\n",s,(void *)((char*)new + sizeof(MemBlock)));
+			return (void *)((char*)new + sizeof(MemBlock));
 		}else
-			curr = curr->next;
-	}while (curr != head);
-
+			curr = curr->prev;
+	}while (curr != head->prev);
+	printf("Saturation\n");
 	return NULL;
 	
 }
-void* memCalloc( unsigned int s){
+void* memCalloc( unsigned int s,char * fileName, unsigned int lineNum){
 	void* ptr = malloc(s);
 	if(ptr != NULL)
 		memset(ptr, 0, s);
 	return ptr;
 }
 
-void* memRealloc(void* p, int s){
+void* memRealloc(void* p, int s, char * fileName, unsigned int lineNum){
 	void *ptr = malloc(s);
 	memcpy(ptr, p, s);
 	free(p);
 	return ptr;
 }
 
-void memfree(void* p){
+void memFree(void* p, char * fileName, unsigned int lineNum){
 
-	MemBlock* ptr, prev;
+	static int initial = 1;
+	static MemBlock *head;
+	MemBlock *ptr, *prev;
 
-	ptr = (MemBlock*) p;
+	
+	if(initial){
+		initial = 0;
+		head = (MemBlock*)memory_block;
+	}
+	if(p < (void*)&memory_block || p > (void*)(&memory_block + MEMBLOCK_SIZE)){
+		printf("pointer %p was not in range\n",p);
+		return;
+	}
+	ptr = (MemBlock*) (p - sizeof (MemBlock));
 
-	if(ptr->prev->next == ptr && !ptr->free){
+	if(ptr->prev != NULL && ptr->prev->next == ptr && !ptr->free){
+		printf("Valid free, freeing block %p\n", p);
 		prev = ptr->prev;
-		next = ptr->next;
+		ptr->next = ptr->next;
 		ptr->free = 1;
-		if(next != head && next->free && next != ptr){
-			ptr->size += sizeof(MemBlock) + next->size;
-			ptr->next = next->next;
+		if(ptr->next != head && ptr->next->free && ptr->next != ptr){
+			printf("Merging to the next block\n");
+			ptr->size += sizeof(MemBlock) + ptr->next->size;
+			ptr->next = ptr->next->next;
 			ptr->next->prev = ptr;
 		}
 
-		if(curr != head && prev->free && prev != ptr){
+		if(ptr != head && prev->free && prev != ptr){
+			printf("Merging with previous block\n");
 			prev->size += sizeof(MemBlock) + ptr->size;
 			prev->next = ptr->next;
 			prev->next->prev = prev;
 		}
-
+	}else{
+		printf("Invalid free or double free\n");
 	}
 
 
--- mymalloc.h
+++ mymalloc.h
@@ -1,23 +1,26 @@
 #ifndef MYMALLOC_H
 #define MYMALLOC_H
 
-#define malloc(x) memMalloc(x);
-#define calloc(x) memCalloc(x);
-#define remalloc(x) memRealloc(x);
-#define free(x) memFree(x);
+#include <stdio.h>
+#include <string.h>
+
+#define malloc(x) memMalloc(x,__FILE__, __LINE__);
+#define calloc(x) memCalloc(x,__FILE__, __LINE__);
+#define remalloc(x) memRealloc(x,__FILE__, __LINE__);
+#define free(x) memFree(x,__FILE__, __LINE__);
 
 
 typedef struct _memBlock MemBlock;
 
 struct _memBlock{
-	MemBlock next, prev;
+	MemBlock *next, *prev;
 	unsigned int size, address;
 	int free;
-}
+};
 
-void* memMalloc(unsigned int s);
-void* memCalloc(unsigned int s);
-void* memRealloc(void* p, int s);
-void memfree(void* p);
+void* memMalloc(unsigned int s, char * fileName, unsigned int lineNum);
+void* memCalloc(unsigned int s, char * fileName, unsigned int lineNum);
+void* memRealloc(void* p, int s,char * fileName, unsigned int lineNum);
+void memFree(void* p, char * fileName, unsigned int lineNum);
 
 #endif
